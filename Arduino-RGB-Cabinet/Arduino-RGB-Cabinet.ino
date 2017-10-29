71adf8b14adbc7b70488198c06d666314d044721
Unknown <fille044@gmail.com>
2017-10-29 18:28:50 +0100

    Started work on wireless functionality

---
+++ Arduino-RGB-Cabinet/Arduino-RGB-Cabinet.ino
@@ -0,0 +1,48 @@
+#include "lib.h"
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+    Serial.begin(115200);
+    delay(1500);
+
+    initIO();
+    initWifi();
+
+    delay(1000);
+}
+
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  delay(20);
+    int clientCounter = 0;
+    // Check if a client has connected
+    client = server.available();
+    if (!client) {
+        return;
+    }
+
+    // Wait until the client sends some data
+    Serial.println("new client");
+    while(!client.available()) {
+        delay(1);
+        clientCounter++;
+        if (clientCounter > 500) {
+          return;
+        }
+    }
+
+    // Read the first line of the request
+    request = client.readStringUntil('\r');
+    Serial.println(request);
+    client.flush();
+
+    runWeb();
+
+
+    delay(1);
+    Serial.println("Ready");
+}
---
+++ Arduino-RGB-Cabinet/lib.h
@@ -0,0 +1,26 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+#define FALSE 0
+#define TRUE 1
+
+// IO pin defines
+#define BLUE 16     // D0 GPIO16
+#define YELLOW 14   // D5 GPIO14
+#define WHITE 15    // D8 GPIO15
+#define PHOTOTRANSISTOR A0
+
+WiFiServer server(80);
+WiFiClient client;
+String request;
+
+/* Public variables */
+const char* ssid = "Molk";
+const char* password = "Molk0901";
+
+/* Public functions */
+void initWifi(void);
+void initIO(void);
+void runWeb(void);
